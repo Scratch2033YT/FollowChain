5756bebaa4a41edd18e38d4b233f6400d52ac39f
Scratch2033YT <162795609+Scratch2033YT@users.noreply.github.com>
2025-02-09 11:51:13 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+localStorage.setItem('prevPage', location.pathname);
+
+document.querySelectorAll('.slider-switch input[type="checkbox"]').forEach((checkbox) => {
+  checkbox.value = 0;
+  checkbox.addEventListener('change', (e) => {
+    e.target.value = e.target.checked ? 1 : 0;
+  });
+});
+
+
+}
